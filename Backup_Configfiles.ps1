2ec359ebc15a6bc27157af61367e05f30d108206
Jayavel Chellapandi Natraj Jayavel <jayavel.natraj@kripya.com>
2025-06-12 21:44:32 +0530

    Backup_logic created

--- Backup_Configfiles.ps1
+++ Backup_Configfiles.ps1
@@ -1,25 +1,24 @@
-# This script backs up three important config files for Centric Wildfly
-
-# Get today’s date in format like 19-05-2025
+$BackupLog = "C:\https_config\file_Backup.log"
 $date = Get-Date -Format "dd-MM-yyyy"
 
-# Create a backup folder like C:\Backup_19-05-2025
-$backupFolder = "C:\Backup_$date"
+function Write-Log {
+    log ([string]$message)
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$timestamp - $message" | Out-File $BackupLog -Append
+}
+
+
+$backupFolder = "C:\https_config\Backup_$date"
 New-Item -ItemType Directory -Path $backupFolder -Force
 
-# Backup each file one by one
-
-# 1. Backup pi-configuration.properties
 Copy-Item "C:\Program Files\Centric Software\C8\Wildfly\standalone\configuration\pi-configuration.properties" `
           "$backupFolder\pi-configuration.properties" -Force
 
-# 2. Backup standalone-pi-mssql.xml
 Copy-Item "C:\Program Files\Centric Software\C8\Wildfly\standalone\configuration\standalone-pi-mssql.xml" `
           "$backupFolder\standalone-pi-mssql.xml" -Force
 
-# 3. Backup standalone.conf.bat
 Copy-Item "C:\Program Files\Centric Software\C8\Wildfly\bin\standalone.conf.bat" `
           "$backupFolder\standalone.conf.bat" -Force
+		  
 
-# Show message
-Write-Host "Backup completed in $backupFolder"
+Write-Log "Backup completed in $backupFolder"
